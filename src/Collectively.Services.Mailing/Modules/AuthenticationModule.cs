115cfdf3998f986c732dae068f94dc69e75192fb
Piotr Gankiewicz <spetz90@gmail.com>
2017-07-21 13:21:21 +0200

    JWT RSA refactoring

--- src/Collectively.Services.Mailing/Modules/AuthenticationModule.cs
+++ src/Collectively.Services.Mailing/Modules/AuthenticationModule.cs
@@ -17,7 +17,7 @@ namespace Collectively.Services.Mailing.Modules
                     return HttpStatusCode.Unauthorized;
                 }
                 
-                return new { token = token.Value };
+                return token.Value;
             });
         }        
     }
